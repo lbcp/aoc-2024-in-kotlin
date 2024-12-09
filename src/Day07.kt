9fc45772de432b875aaa4dad548a55a0f74a0af9
Micha <michael.fichtner@hotmail.de>
2024-12-09 23:59:51 +0100

    Day 7 part 1 done. Part 2 has an error.

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,59 +1,131 @@
 import java.io.File
 
 fun main() {
-    fun readInput(filename: String): Pair<MutableList<Int>, MutableList<List<Int>>> {
-        val sum = mutableListOf<Int>()
-        val values = mutableListOf<List<Int>>()
+    fun readInput(filename: String): Pair<MutableList<Long>, MutableList<List<Long>>> {
+        val sum = mutableListOf<Long>()
+        val values = mutableListOf<List<Long>>()
         File(filename).useLines { lines ->
             lines.forEach {
-                sum.add(it.split(":")[0].toInt())
-                values.add(it.split(":")[1].trim().split(" ").map { num -> num.toInt() })
+                sum.add(it.split(":")[0].toLong())
+                values.add(it.split(":")[1].trim().split(" ").map { num -> num.toLong() })
             }
         }
         return Pair(sum, values)
     }
 
-    fun part1(sums: MutableList<Int>, values: MutableList<List<Int>>): Int {
-        val maxOperators = sums.size - 1
+    fun part1(sums: MutableList<Long>, values: MutableList<List<Long>>): Long {
+        var maxOperators = 0
+        values.forEach { if (it.size > maxOperators) maxOperators = it.size - 1 }
+        val correctSums = mutableSetOf<Long>()
+        val correctMap = mutableMapOf<List<Long>, Long>()
+        var totalSum: Long = 0
 
-        fun calculateResults(operators: List<Char>, valuesList: List<Int> ): Int {
+        fun calculateResults(operators: List<Char>, valuesList: List<Long> ): Long {
             // TODO: Finish this part.
             // I now need to calculate with the operators.
             var result = valuesList[0]
+            for (i in 1 until valuesList.size) {
+                when (operators[i-1]) {
+                    '+' -> result += valuesList[i]
+                    '*' -> result *= valuesList[i]
+                }
+            }
             return result
         }
 
         fun iterateCombinations(operatorList: List<Char>) {
-            // TODO: Check the results of the operations.
             if (operatorList.size == maxOperators)  {
+                println(operatorList)
                 for (i in values.indices) {
-                    if (sums[i] == calculateResults(operatorList, values[i])) return
+                    if (sums[i] == calculateResults(operatorList, values[i])) {
+                        correctSums.add(sums[i])
+                        correctMap[values[i]] = sums[i]
+                    }
+                }
+                return
+            } else {
+                iterateCombinations(operatorList + listOf('+'))
+                iterateCombinations(operatorList + listOf('*'))
+            }
+        }
+        iterateCombinations(emptyList())
+        return correctMap.values.sum()
+    }
+
+    fun part2(sums: MutableList<Long>, values: MutableList<List<Long>>): Long {
+        var maxOperators = 0
+        values.forEach { if (it.size > maxOperators) maxOperators = it.size - 1 }
+        val correctSums = mutableSetOf<Long>()
+        val correctMap = mutableMapOf<List<Long>, Long>()
+        var totalSum: Long = 0
+
+        fun calculateResults(listOfOperators: List<Char>, listOfValues: List<Long> ): Long {
+            // For Part 2 I first need to combine the numbers and purge the | operator
+            var operators = mutableListOf<Char>()
+            var valuesList = mutableListOf<Long>()
+            if ('|' in listOfOperators) {
+                operators = listOfOperators.filter { it != '|'}.toMutableList()
+                var j = 0
+                for (i in listOfValues.indices) {
+                    println(j)
+                    if (j >= listOfValues.size) break
+                    if (listOfOperators[i] == '|' && listOfValues.size-1 >= j+1) {
+                        valuesList.add((listOfValues[j].toString() + listOfValues[j+1].toString()).toLong())
+                        j += 2
+                    } else {
+                        valuesList.add(listOfValues[j])
+                        j += 1
+                    }
+                }
+            } else {
+                operators = listOfOperators.toMutableList()
+                valuesList = listOfValues.toMutableList()
+            }
+            println(listOfValues)
+            println(listOfOperators)
+            println(valuesList)
+            println(operators)
+
+            var result = valuesList[0]
+            for (i in 1 until valuesList.size) {
+                when (operators[i-1]) {
+                    '+' -> result += valuesList[i]
+                    '*' -> result *= valuesList[i]
+                    '|' -> continue
                 }
             }
-            iterateCombinations(operatorList + listOf('+'))
-            iterateCombinations(operatorList + listOf('*'))
-
+            return result
         }
 
-
-        return 0
+        fun iterateCombinations(operatorList: List<Char>) {
+            if (operatorList.size == maxOperators + 1) {
+                for (i in values.indices) {
+                    if (sums[i] == calculateResults(operatorList, values[i])) {
+                        correctSums.add(sums[i])
+                        correctMap[values[i]] = sums[i]
+                    }
+                }
+                return
+            } else if (operatorList.size == maxOperators) {
+                // the last operator must not be an |
+                iterateCombinations(operatorList + listOf('+'))
+                iterateCombinations(operatorList + listOf('*'))
+            } else {
+                iterateCombinations(operatorList + listOf('+'))
+                iterateCombinations(operatorList + listOf('*'))
+                iterateCombinations(operatorList + listOf('|'))
+            }
+        }
+        iterateCombinations(emptyList())
+        println(correctMap)
+        return correctMap.values.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    //check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val (sums, values) = readInput("src/TestInput_Day7.txt")
-    println(sums)
-    println(values)
     //check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     //val input = readInput("Day01")
-    //part1(input).println()
-    //part2(input).println()
+    part1(sums, values).println()
+    part2(sums, values).println()
 }
