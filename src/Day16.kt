eaef4b711f6def2749254c41323e32c1558aa107
lbcp <michael.fichtner@hotmail.de>
2025-04-10 11:25:08 +0200

    Day 16 Part 1 and Part 2 finished together. Used a modified Dijkstra algorithm

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,5 +1,5 @@
 import java.io.File
-import kotlin.math.abs
+import java.util.PriorityQueue
 import kotlin.time.measureTime
 
 fun main() {
@@ -16,83 +16,140 @@ fun main() {
         return input
     }
 
+    //class Task(val functionCall: Unit, val score: Int)
+    class Task(private val functionInitializer: () -> () -> Unit, val score: Int) {
+        val functionCall: () -> Unit by lazy { functionInitializer() }
+    }
+
+
     fun part1(map: MutableList<MutableList<String>>): Int {
-        // It takes too much time.
-        // I think I should implement a priority queue that adds 1000 to each turn, so that I do not have to get
-        // every route.
-        val roads = mutableMapOf<Int, Pair<MutableList<List<Int>>, Int>>()
+        // I'll use some kind of poor mens Dijkstra algorithm
+        // Since the direction makes a difference I generated a rather expressive cost map for the nodes.
+        val costMap = mapOf<String, MutableMap<List<Int>, Int>>(
+            "north" to mutableMapOf<List<Int>, Int>(),
+            "south" to mutableMapOf<List<Int>, Int>(),
+            "west" to mutableMapOf<List<Int>, Int>(),
+            "east" to mutableMapOf<List<Int>, Int>(),
+        )
+
+        var bestScore: Int = Int.MAX_VALUE
+        val pathsFound = mutableMapOf<Int, MutableList<MutableList<List<Int>>>>()
+        val pathQueue = PriorityQueue<Task>(compareBy { it.score })
 
         fun stepForward(
             map: MutableList<MutableList<String>>, pos: List<Int>, direction: String,
-            pathTaken: MutableList<List<Int>>, turns: Int
+            pathTaken: MutableList<List<Int>>, score: Int
         ) {
             val curPath = pathTaken.toMutableList()
             val row = pos[0]
             val col = pos[1]
-            val curIndex = roads.size
-            if (map[row][col] == "E") roads[curIndex] = Pair(curPath, turns)
+            val directionMap = mapOf(
+                "north" to listOf(row-1, col),
+                "south" to listOf(row + 1, col),
+                "west" to listOf(row, col - 1),
+                "east" to listOf(row, col + 1))
+            // val curIndex = roads.size
+            // Exit conditions
 
             curPath.add(pos)
+            if (map[row][col] == "E") {
+                if (bestScore >= score) {
+                    bestScore = score
+                    val scorePath = pathsFound.getOrPut(score, { mutableListOf() })
+                    scorePath.add(curPath)
+                }
+                return
+            }
+            if (costMap[direction]!![listOf(row, col)]!! < score ) {
+                return
+            } else if (costMap[direction]!![listOf(row, col)]!! >= score ) {
+                costMap[direction]?.set(listOf(row, col), score)
+            }
+
+            val possibleDirections = mutableListOf<String>()
             if (listOf(row - 1, col) !in pathTaken && map[row - 1][col] == "." || map[row - 1][col] == "E") {
-                var newTurns = turns
-                if (direction != "north") newTurns += 1
-                stepForward(map, listOf(row - 1, col), "north", curPath, newTurns)
-            }
-            if (listOf(row + 1, col) !in pathTaken && map[row + 1][col] == "." || map[row + 1][col] == "E") {
-                var newTurns = turns
-                if (direction != "south") newTurns += 1
-                stepForward(map, listOf(row + 1, col), "south", curPath, newTurns)
-            }
-            if (listOf(row, col - 1) !in pathTaken && map[row][col - 1] == "." || map[row][col - 1] == "E") {
-                var newTurns = turns
-                if (direction != "west") newTurns += 1
-                stepForward(map, listOf(row, col - 1), "west", curPath, newTurns)
+                possibleDirections.add("north")
             }
             if (listOf(row, col + 1) !in pathTaken && map[row][col + 1] == "." || map[row][col + 1] == "E") {
-                var newTurns = turns
-                if (direction != "east") newTurns += 1
-                stepForward(map, listOf(row, col + 1), "east", curPath, newTurns)
+                possibleDirections.add("east")
+            }
+            if (listOf(row + 1, col) !in pathTaken && map[row + 1][col] == "." || map[row + 1][col] == "E") {
+                possibleDirections.add("south")
+            }
+            if (listOf(row, col - 1) !in pathTaken && map[row][col - 1] == "." || map[row][col - 1] == "E") {
+                possibleDirections.add("west")
+            }
+
+
+            if (direction in possibleDirections) {
+                val newScore = score + 1
+                pathQueue.add(Task(functionInitializer = {
+                    {
+                        stepForward(map,directionMap[direction]!!,direction, curPath, newScore)
+                    }
+                },
+                    newScore))
+                possibleDirections.remove(direction)
+            }
+            for (dir in possibleDirections) {
+                val newScore = score + 1001
+                pathQueue.add(Task(functionInitializer = {
+                    {
+                        stepForward(map,directionMap[dir]!!, dir, curPath, newScore)
+                    }
+                },
+                    newScore))
             }
         }
-        // The idea is that I generate a List with the path. If I pass through a wall, I only have to check,
-        // where in the list I am and then calculate the number of remaining tiles.
+
         var startPos = listOf<Int>()
         for ((rowNo, row) in map.withIndex()) {
             for ((colNo, tile) in row.withIndex()) {
-                if (tile == "S") startPos = listOf(rowNo, colNo)
+                costMap["north"]!![listOf(rowNo, colNo)] = Int.MAX_VALUE
+                costMap["south"]!![listOf(rowNo, colNo)] = Int.MAX_VALUE
+                costMap["west"]!![listOf(rowNo, colNo)] = Int.MAX_VALUE
+                costMap["east"]!![listOf(rowNo, colNo)] = Int.MAX_VALUE
+                if (tile == "S") {
+                    startPos = listOf(rowNo, colNo)
+                    costMap["east"]!![listOf(rowNo, colNo)] = 0
+                }
             }
         }
-        stepForward(map, startPos, "east", mutableListOf<List<Int>>(), 0)
-        var scores = mutableListOf<Int>()
-        roads.forEach { (key, value) ->
-            scores.add(value.first.size - 1 + (value.second * 1000))
+
+        val initTask = Task(functionInitializer = {
+            {
+                stepForward(
+                    map,
+                    startPos,
+                    "east",
+                    mutableListOf<List<Int>>(),
+                    0
+                )
+            } }, 0)
+        pathQueue.add(initTask)
+
+        while (pathQueue.isNotEmpty()) {
+            if (pathQueue.peek().score > bestScore) break
+            val runTask = pathQueue.poll() ?: break
+            runTask.functionCall()
         }
-        println(scores.sorted())
-        return scores.min()
+
+        val bestTiles = mutableSetOf<List<Int>>()
+        pathsFound[bestScore]!!.forEach { bestTiles.addAll(it) }
+        println("Number of best tiles in Maze: ${bestTiles.size}")
+        return bestScore
     }
 
-    fun part2(map: MutableList<MutableList<String>>): Int {
-        // Part 2 theory.
-        // I "only" need to find all tiles that are within 20 steps to all directions.
-        // I could "draw" a diamond around my current position to isolate the reachable positions
-
-        return 0
-    }
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("src/TestInput_Day16.txt")
-    val timeTakenT1 = measureTime {  check(part1(testInput) == 7036) }
+    val timeTakenT1 = measureTime { check(part1(testInput) == 7036) }
+    println("Test 1 took: $timeTakenT1")
 
     val testInput2 = readInput("src/TestInput_Day16_2.txt")
     val timeTakenT2 = measureTime { check(part1(testInput2) == 11048) }
-    println("PartT1 took: $timeTakenT1")
-    println("PartT2 took: $timeTakenT2")
+    println("Test 2 took: $timeTakenT2")
 
     val input = readInput("src/Input_Day16.txt")
     val timeTaken1 = measureTime { part1(input).println() }
-    println("Part1 took: $timeTaken1")
-    /*
-    val timeTaken2 = measureTime { part2(input).println() }
-    println("Part2 took: $timeTaken2")
-     */
+    println("Full task took: $timeTaken1")
+
 }
